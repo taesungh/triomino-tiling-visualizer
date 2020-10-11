98129fc13e862a133f0b60e7f7ee7e178e70cc11
Taesung Hwang <taesunghwang@yahoo.com>
2020-10-11 01:15:55 -0700

    Updates

---
+++ precache-manifest.02d167719b2484b0c537ee1b67304ec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98e51b7b4280f1047bb3816b3fd1f6b",
+    "url": "/triomino-tiling-visualizer/index.html"
+  },
+  {
+    "revision": "51f22d933353e0f84248",
+    "url": "/triomino-tiling-visualizer/static/css/main.91617c18.chunk.css"
+  },
+  {
+    "revision": "c5ef89b4b96837329f53",
+    "url": "/triomino-tiling-visualizer/static/js/2.64be495c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/triomino-tiling-visualizer/static/js/2.64be495c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f22d933353e0f84248",
+    "url": "/triomino-tiling-visualizer/static/js/main.46b3d0f6.chunk.js"
+  },
+  {
+    "revision": "aced5fc711d585e2ed7f",
+    "url": "/triomino-tiling-visualizer/static/js/runtime-main.a279b5bd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2dee9a5a599eb053f4d98d168fa46bb.js"
+  "/triomino-tiling-visualizer/precache-manifest.02d167719b2484b0c537ee1b67304ec8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/triomino-tiling-visualizer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
