7ad1518b07ee716ef9ce08941b6accc433bed003
Taesung Hwang <taesunghwang@yahoo.com>
2020-10-11 01:13:19 -0700

    Updates

---
+++ precache-manifest.b2dee9a5a599eb053f4d98d168fa46bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f800017e35d1321d2a45e9634956a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07ee888335eb4ba79135",
+    "url": "/static/css/main.91617c18.chunk.css"
+  },
+  {
+    "revision": "c5ef89b4b96837329f53",
+    "url": "/static/js/2.64be495c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64be495c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ee888335eb4ba79135",
+    "url": "/static/js/main.c7858d51.chunk.js"
+  },
+  {
+    "revision": "f414119f48316c379ed5",
+    "url": "/static/js/runtime-main.85afaa3c.js"
+  }
+]);
